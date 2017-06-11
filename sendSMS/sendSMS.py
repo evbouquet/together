071b32870a19809db5912704e1cad7e3af5da445
root <root@localhost.localdomain>
2017-06-10 22:10:11 -0700

    small change

--- sendSMS/sendSMS.py
+++ sendSMS/sendSMS.py
@@ -7,14 +7,14 @@ response = client.send_message(
 {
 'from': '12033189172',
 'to': '14088591938', 
-'text': 'Once upon a time, there was a magical Narwhal. Narwhal was the cutest and cuddliest of all Narwhals, and everyone loved this fluffy cutie Narwhal. Narwhal especially loved singing songs with his friendly friend Sheepie. Together they were the cutest and floofiest and squishiest of friends!!!'
+'type': 'unicode',
+'text': ' Frpm 大象 Once upon a time, there was a magical Narwhal. Narwhal was the cutest and cuddliest of all Narwhals, and everyone loved this fluffy cutie Narwhal. Narwhal especially loved singing songs with his friendly friend Sheepie. Together they were the cutest and floofiest and squishiest of friends!!!'
 })
 
 response = response['messages'][0]
 
 if response['status'] == '0':
   print 'Sent message', response['message-id']
-
   print 'Remaining balance is', response['remaining-balance']
 else:
   print 'Error:', response['error-text']
