491e37b183851b3446530153ba0abb01f3d10f47
Jocelin Su <jsu@localhost.localdomain>
2017-06-11 13:59:06 -0700

    I love sheeepppiiieee

--- sendSMS/sendSMS.py
+++ sendSMS/sendSMS.py
@@ -8,7 +8,7 @@ response = client.send_message(
 'from': '12033189172',
 'to': '14088591938', 
 'type': 'unicode',
-'text': ' Frpm 大象 Once upon a time, there was a magical Narwhal. Narwhal was the cutest and cuddliest of all Narwhals, and everyone loved this fluffy cutie Narwhal. Narwhal especially loved singing songs with his friendly friend Sheepie. Together they were the cutest and floofiest and squishiest of friends!!!'
+'text': ' From 大象 Once upon a time, there was a magical Narwhal. Narwhal was the cutest and cuddliest of all Narwhals, and everyone loved this fluffy cutie Narwhal. Narwhal especially loved singing songs with his friendly friend Sheepie. Together they were the cutest and floofiest and squishiest of friends!!!'
 })
 
 response = response['messages'][0]
