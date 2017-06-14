e5580d5f48f85e618b7949b4f6abe61d28b73595
jiaxiangsu <jiaxiang@gmail.com>
2017-06-14 12:43:33 +0800

    sendSMS.py

--- sendSMS/sendSMS.py
+++ sendSMS/sendSMS.py
@@ -5,17 +5,23 @@ import nexmo
 client = nexmo.Client(key='2ff54825', secret='bf2a896456351da1')
 response = client.send_message(
 {
-'from': '12033189172',
-'to': '14088591938', 
+
+ 'from': '12033189172',
+# US number
+ 'to': '14088591938',
+# China number
+# 'to': '8613161503275',
+
 'type': 'unicode',
-'text': ' From 大象 Once upon a time, there was a magical Narwhal. Narwhal was the cutest and cuddliest of all Narwhals, and everyone loved this fluffy cutie Narwhal. Narwhal especially loved singing songs with his friendly friend Sheepie. Together they were the cutest and floofiest and squishiest of friends!!!'
+#'text': 'Would you want to be a billonaire in your lifetime? Call this number and find out how.'
+'text': '明月如霜，好风如水，清景无限。曲港跳鱼，圆荷泻露，寂寞无人见。紞如三鼓，铿然一叶，黯黯梦云惊断。夜茫茫，重寻无处，觉来小园行遍。天涯倦客，山中归路，望断故园心眼。燕子楼空，佳人何在，空锁楼中燕。古今如梦，何曾梦觉，但有旧欢新怨。异时对，黄楼夜景，为余浩叹。'
 })
 
 response = response['messages'][0]
 
 if response['status'] == '0':
-  print 'Sent message', response['message-id']
-  print 'Remaining balance is', response['remaining-balance']
+  print ('Sent message', response['message-id'])
+  print ('Remaining balance is', response['remaining-balance'])
 else:
-  print 'Error:', response['error-text']
+  print ('Error:', response['error-text'])
 
